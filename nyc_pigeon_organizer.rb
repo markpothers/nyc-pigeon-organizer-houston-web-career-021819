7b21f952fd950ffd32169a5787c4d24b2340eacb
Mark Pothecary <markpothers@hotmail.com>
2019-02-15 23:20:11 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+org_data = {}
+    data.each do |attributes, attribute|
+      attribute.each do |option, names|
+        names.each do |name|
+          org_data[name] = {:color => [], :gender => [], :lives => []}
+        end
+      end
+    end
+
+    org_data.each do |name, attributes|
+      attributes.each do |attribute, value|
+        data.each do |orig_attributes, orig_attribute|
+          orig_attribute.each do |orig_attribute, names|
+            if names.include?(name) && attribute == orig_attributes
+              value << orig_attribute.to_s
+            end
+          end
+        end
+      end
+    end
+org_data
 end
